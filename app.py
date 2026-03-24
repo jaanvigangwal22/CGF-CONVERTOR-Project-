9e2e79c5ac918c08b25f0ec9eb52f6fb828b8036
jaanvigangwal22 <jaanvi.gangwal04@gmail.com>
2026-03-24 16:54:25 +0530

    Added input display feature

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
 
+    grammar = ""
+
     if request.method == "POST":
         grammar = request.form["grammar"]
-        print("User Input:", grammar)
 
-    return render_template("index.html")
+    return render_template("index.html", grammar=grammar)
 
 if __name__ == "__main__":
     app.run(debug=True)
